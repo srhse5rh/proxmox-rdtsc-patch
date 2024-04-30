7fcc036b1ed282c0ddc8605760a45c7ecc3e51d1
srhse5rh <148304527+srhse5rh@users.noreply.github.com>
2024-04-30 09:26:38 +0800

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,97 +1,113 @@
 #!/bin/bash
 
-# linux_git_repo="https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git"
-# linux_git_branch="linux-rolling-lts"
-# cpu_brand=$(grep -m 1 'vendor_id' /proc/cpuinfo | cut -c13-)
+echo '
+/* EDITED BY PATCH */
+static u32 print_once = 1;
+static int handle_rdtsc_interception(struct kvm_vcpu *vcpu)
+{
+	static u64 rdtsc_fake = 0;
+	static u64 rdtsc_prev = 0;
+	u64 rdtsc_real = rdtsc();
+	if(print_once)
+	{
+		printk("[handle_rdtsc] fake rdtsc svm function is working");
+		print_once = 0;
+		rdtsc_fake = rdtsc_real;
+	}
 
-# git clone --branch $linux_git_branch $linux_git_repo
+	if(rdtsc_prev != 0)
+	{
+		if(rdtsc_real > rdtsc_prev)
+		{
+			u64 diff = rdtsc_real - rdtsc_prev;
+			u64 fake_diff =  diff / 20; // if you have 3.2Ghz on your vm, change 20 to 16
+			rdtsc_fake += fake_diff;
+		}
+	}
+	if(rdtsc_fake > rdtsc_real)
+	{
+		rdtsc_fake = rdtsc_real;
+	}
+	rdtsc_prev = rdtsc_real;
 
-if [ -z "$(grep -r "EDITED BY SED" "$(pwd)/submodules/ubuntu-kernel/arch/x86/kvm/svm/svm.c")" ]; then
-        line_1=$(($(grep -n "kvm_handle_invpcid(vcpu, type, gva);" submodules/ubuntu-kernel/arch/x86/kvm/svm/svm.c | cut -f1 -d ':') + 2))
-        sed -i "${line_1}a\
-\
-/* EDITED BY SED */\
-static u32 print_once = 1;\
-static int handle_rdtsc_interception(struct kvm_vcpu *vcpu)\
-{\
-	static u64 rdtsc_fake = 0;\
-	static u64 rdtsc_prev = 0;\
-	u64 rdtsc_real = rdtsc();\
-	if(print_once)\
-	{\
-		printk(\"[handle_rdtsc] fake rdtsc svm function is working\");\
-		print_once = 0;\
-		rdtsc_fake = rdtsc_real;\
-	}\
-\
-	if(rdtsc_prev != 0)\
-	{\
-		if(rdtsc_real > rdtsc_prev)\
-		{\
-			u64 diff = rdtsc_real - rdtsc_prev;\
-			u64 fake_diff =  diff / 20; // if you have 3.2Ghz on your vm, change 20 to 16\
-			rdtsc_fake += fake_diff;\
-		}\
-	}\
-	if(rdtsc_fake > rdtsc_real)\
-	{\
-		rdtsc_fake = rdtsc_real;\
-	}\
-	rdtsc_prev = rdtsc_real;\
-\
-	vcpu->arch.regs[VCPU_REGS_RAX] = rdtsc_fake & -1u;\
-	vcpu->arch.regs[VCPU_REGS_RDX] = (rdtsc_fake >> 32) & -1u;\
-\
-	return svm_skip_emulated_instruction(vcpu);\
-}" "$(pwd)/submodules/ubuntu-kernel/arch/x86/kvm/svm/svm.c"
+	vcpu->arch.regs[VCPU_REGS_RAX] = rdtsc_fake & -1u;
+	vcpu->arch.regs[VCPU_REGS_RDX] = (rdtsc_fake >> 32) & -1u;
 
-        line_2=$(grep -n "svm_set_intercept(svm, INTERCEPT_RSM);" submodules/ubuntu-kernel/arch/x86/kvm/svm/svm.c | cut -f1 -d ':')
-        sed -i "${line_2}a\
-    svm_set_intercept(svm, INTERCEPT_RDTSC);" "$(pwd)/submodules/ubuntu-kernel/arch/x86/kvm/svm/svm.c"
-        line_3=$(grep -n "SVM_EXIT_VMGEXIT" submodules/ubuntu-kernel/arch/x86/kvm/svm/svm.c | cut -f1 -d ':')
-        sed -i "${line_3}a\
-	[SVM_EXIT_RDTSC]			= handle_rdtsc_interception" "$(pwd)/submodules/ubuntu-kernel/arch/x86/kvm/svm/svm.c"
-fi
+	return svm_skip_emulated_instruction(vcpu);
+}
 
-if [ -z $(grep -r "EDITED BY SED" "$(pwd)/submodules/ubuntu-kernel/arch/x86/kvm/vmx/vmx.c") ]; then
+static u32 print_once = 1;
+static int handle_rdtscp_interception(struct kvm_vcpu *vcpu)
+{
+	static u64 rdtsc_fake = 0;
+	static u64 rdtsc_prev = 0;
+	u64 rdtsc_real = rdtsc();
+	if(print_once)
+	{
+		printk("[handle_rdtsc] fake handle_rdtscp svm function is working");
+		print_once = 0;
+		rdtsc_fake = rdtsc_real;
+	}
 
-        sed -i "5984a\
-\
-/* EDITED BY SED */\
-static u32 print_once = 1;\
-static int handle_rdtsc(struct kvm_vcpu *vcpu)\
-{\
-        static u64 rdtsc_fake = 0;\
-        static u64 rdtsc_prev = 0;\
-        u64 rdtsc_real = rdtsc();\
-        if(print_once)\
-        {\
-                printk(\"[handle_rdtsc] fake rdtsc vmx function is working\");\
-                print_once = 0;\
-                rdtsc_fake = rdtsc_real;\
-        }\
-\
-        if(rdtsc_prev != 0)\
-        {\
-                if(rdtsc_real > rdtsc_prev)\
-                {\
-                        u64 diff = rdtsc_real - rdtsc_prev;\
-                        u64 fake_diff =  diff / 16; // if you have 4.2Ghz on your vm, change 16 to 20\
-                        rdtsc_fake += fake_diff;\
-                }\
-        }\
-        if(rdtsc_fake > rdtsc_real)\
-        {\
-                rdtsc_fake = rdtsc_real;\
-        }\
-        rdtsc_prev = rdtsc_real;\
-\
-        vcpu->arch.regs[VCPU_REGS_RAX] = rdtsc_fake & -1u;\
-        vcpu->arch.regs[VCPU_REGS_RDX] = (rdtsc_fake >> 32) & -1u;\
-\
-        return skip_emulated_instruction(vcpu);\
-}" "$(pwd)/submodules/ubuntu-kernel/arch/x86/kvm/vmx/vmx.c"
+	if(rdtsc_prev != 0)
+	{
+		if(rdtsc_real > rdtsc_prev)
+		{
+			u64 diff = rdtsc_real - rdtsc_prev;
+			u64 fake_diff =  diff / 20; // if you have 3.2Ghz on your vm, change 20 to 16
+			rdtsc_fake += fake_diff;
+		}
+	}
+	if(rdtsc_fake > rdtsc_real)
+	{
+		rdtsc_fake = rdtsc_real;
+	}
+	rdtsc_prev = rdtsc_real;
 
-        sed -i "6076a\
-        [EXIT_REASON_RDTSC]                   = handle_rdtsc," "$(pwd)/submodules/ubuntu-kernel/arch/x86/kvm/vmx/vmx.c"
-fi
+	vcpu->arch.regs[VCPU_REGS_RAX] = rdtsc_fake & -1u;
+	vcpu->arch.regs[VCPU_REGS_RDX] = (rdtsc_fake >> 32) & -1u;
+
+	return svm_skip_emulated_instruction(vcpu);
+}
+
+static int handle_umwait_interception(struct kvm_vcpu *vcpu)
+{
+	kvm_skip_emulated_instruction(vcpu);
+	return 1;
+}
+
+static int handle_tpause_interception(struct kvm_vcpu *vcpu)
+{
+	kvm_skip_emulated_instruction(vcpu);
+	return 1;
+}
+' >svm-patch-1
+
+echo '
+	/* EDITED BY PATCH */
+	svm_set_intercept(svm, INTERCEPT_RDTSC);
+	svm_set_intercept(svm, INTERCEPT_RDSCP);
+	svm_set_intercept(svm, INTERCEPT_UMWAIT);
+	svm_set_intercept(svm, INTERCEPT_TPAUSE);
+' >svm-patch-2
+
+echo '
+	/* EDITED BY PATCH */
+	[EXIT_REASON_RDTSC]			= handle_rdtsc_interception,
+	[EXIT_REASON_RDTSCP]			= handle_rdtscp_interception,
+	[EXIT_REASON_UMWAIT]			= handle_umwait_interception,
+	[EXIT_REASON_TPAUSE]			= handle_tpause_interception,
+' >svm-patch-3
+
+svmfile="submodules/ubuntu-kernel/arch/x86/kvm/svm/svm.c"
+vmxfile="submodules/ubuntu-kernel/arch/x86/kvm/vmx/vmx.c"
+
+sed -i '/return kvm_handle_invpcid/{
+    n
+    n
+    r svm-patch-1
+}' ${svmfile}
+
+sed -i '/svm_set_intercept(svm, INTERCEPT_RSM)/r svm-patch-2' ${svmfile}
+
+sed -i '/SVM_EXIT_VMGEXIT/r svm-patch-3' ${svmfile}
