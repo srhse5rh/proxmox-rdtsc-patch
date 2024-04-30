9bc7c57102d3bd3c39416e2a0f499f36fdb39448
srhse5rh <148304527+srhse5rh@users.noreply.github.com>
2024-04-30 20:21:18 +0800

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -29,35 +29,6 @@ static int handle_rdtsc(struct kvm_vcpu *vcpu)
 
 	return svm_skip_emulated_instruction(vcpu);
 }
-
-static u32 print_once = 1;
-static int handle_rdtscp(struct kvm_vcpu *vcpu)
-{
-	static u64 rdtsc_fake = 0;
-	static u64 rdtsc_prev = 0;
-	u64 rdtsc_real = rdtsc();
-	printk_once("[handle_rdtsc] fake rdtscp svm function is working\n");
-
-	if(rdtsc_prev != 0)
-	{
-		if(rdtsc_real > rdtsc_prev)
-		{
-			u64 diff = rdtsc_real - rdtsc_prev;
-			u64 fake_diff =  diff / 16; // if you have 3.2Ghz on your vm, change 20 to 16
-			rdtsc_fake += fake_diff;
-		}
-	}
-	if(rdtsc_fake > rdtsc_real)
-	{
-		rdtsc_fake = rdtsc_real;
-	}
-	rdtsc_prev = rdtsc_real;
-
-	vcpu->arch.regs[VCPU_REGS_RAX] = rdtsc_fake & -1u;
-	vcpu->arch.regs[VCPU_REGS_RDX] = (rdtsc_fake >> 32) & -1u;
-
-	return svm_skip_emulated_instruction(vcpu);
-}
 /* EDITED BY PATCH */
 ' >svm-patch-1
 
